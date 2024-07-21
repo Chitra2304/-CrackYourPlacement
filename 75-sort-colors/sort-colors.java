04d2cdc85d3e4d1f7aaf431a720048a53fa3d4cc
Chitra Pandey <139427298+Chitra2304@users.noreply.github.com>
2024-07-21 20:42:08 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (39.65%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,27 +1,37 @@
-// two pass : counting occurrences of 3 colours and overwriting the nums array
+// one pass :  Dutch National Flag problem algorithm
+// 3 pointer
 // Time complexity: O(n)
 // Space complexity: O(1)
 
 class Solution {
     public void sortColors(int[] nums) {
-        int[] counting = new int[3];
-        for(int x : nums){    //counting occurrences
-            counting[x]++;
-        }
-
-        int j = 0;           //overwriting the nums array
-        for(int i=0; i<3; i++){
-            while(counting[i]>0){
-                nums[j] = i;
-                j++;
-                counting[i]--;
+        int n = nums.length;
+        int l = 0;
+        int r = n-1;
+        int id = 0;
+        while(id<=r){
+            if(nums[id]==0){              // id can only be 0 and 2
+                int temp = nums[l];
+                nums[l] = nums[id];
+                nums[id] = temp;
+                l++;
+                id++;
+            }
+            else if(nums[id]==2){
+                int temp = nums[r];
+                nums[r] = nums[id];
+                nums[id] = temp;
+                r--;
+            }
+            else{                         // if id is 1
+                id++;
             }
         }
     }
 }
 
 
+// https://youtu.be/yhlerbLPrOs?si=eldcwi1VldRuncrx
 
-// two pass solution:
-// it effectively makes two passes over the array, resulting in an overall time complexity of O(2n), which simplifies to 
-// O(n), but it's not a single pass.
+
+// The Dutch National Flag problem is a popular computer science problem proposed by Edsger Dijkstra. It involves sorting an array containing three different types of elements (often represented as 0, 1, and 2) in linear time and with a constant amount of space. The goal is to rearrange the elements so that all elements of the same type are adjacent, with the order of the types following a specified sequence (e.g., 0s, then 1s, then 2s).
