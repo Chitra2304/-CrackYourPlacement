2634eb2c5f7d5be1c3ff55f6423e1bed1c24995a
Chitra Pandey <139427298+Chitra2304@users.noreply.github.com>
2024-07-23 21:59:30 +0530

    Time: 5 ms (65.63%) | Memory: 58.3 MB (40.54%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -1,17 +1,38 @@
-// counting frequency of each number in nums[]
+// ways :
+// 1. Brute Force (gives TLE) : Time:O(n^2)   Space:O(1)
+// 2. Sorting : Time:O(n*logn)   Space:O(1)
+// 3. HashSet : Time:O(n)   Space:O(n)
+// 4. Boolean array : Time:O(n)   Space:O(n)
+// 5. Mark visited element negative : Time:O(n)   Space:O(1)
+// 6. Counting frequency : Time:O(n)   Space:O(n)
+// 7. Binary search : Time:O(nlogn)    Space:O(1)
+// 8. Bit manipulation : Time:O(nlogn)    Space:O(1)
+// but all these are either modifying the array or not using constant extra space
 
+// 9. Fast and Slow Pointers : Time:O(n)   Space:O(1)
+// this approach 9 satisfies all the restrictions
+
+
+
+//Learn (Fast and Slow Pointers) --->(Hare and Tortoise Algo)---> (Cycle detection in linked list)
 class Solution {
     public int findDuplicate(int[] nums) {
-        int[] integers = new int[nums.length];
-        for(int i=0; i<nums.length; i++){
-            integers[nums[i]]++;
+        int slow = 0;
+        int fast = 0;
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+        slow = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
         }
-
-        for(int i=1; i<=nums.length; i++){
-            if(integers[i] > 1){
-                return i;
-            }
-        }
-        return -1;
+        return slow;
     }
 }
+
+
+
+// https://youtu.be/wiOo4DC5GGA?si=LlIgs66jt6gYNehf ---> for ll cycle detection
+// https://youtu.be/qNx2nKBdvwU?si=suF-6ShkzjcPFbJP
